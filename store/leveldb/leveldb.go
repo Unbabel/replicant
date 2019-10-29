a62302603ea6bf3afc4f89d0a8a8d03903714651
Bruno Moura <brunotm@gmail.com>
2019-10-29 23:43:42 +0000

    add leveldb store

---
+++ store/leveldb/leveldb.go
@@ -0,0 +1,105 @@
+package leveldb
+
+import (
+	"encoding/json"
+
+	"github.com/brunotm/replicant/transaction"
+	"github.com/brunotm/replicant/transaction/manager"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+var _ manager.Store = (*Store)(nil)
+
+// Store is a in memory transaction config store
+type Store struct {
+	data *leveldb.DB
+}
+
+// New creates a new in memory transaction config store
+func New(path string) (s *Store, err error) {
+	s = &Store{}
+
+	s.data, err = leveldb.OpenFile(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+// Close the store
+func (s *Store) Close() (err error) {
+	return s.data.Close()
+}
+
+// Has checks if a transaction config exists under the given name
+func (s *Store) Has(name string) (exists bool, err error) {
+	return s.data.Has([]byte(name), nil)
+}
+
+// Get a transaction config from the store
+func (s *Store) Get(name string) (config transaction.Config, err error) {
+	b, err := s.data.Get([]byte(name), nil)
+
+	switch {
+	case err == leveldb.ErrNotFound:
+		return config, transaction.ErrTransactionNotFound
+	case err != nil:
+		return config, err
+	}
+
+	err = json.Unmarshal(b, &config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+
+}
+
+// Set stores the given transaction config
+func (s *Store) Set(name string, config transaction.Config) (err error) {
+
+	b, err := json.Marshal(&config)
+	if err != nil {
+		return err
+	}
+
+	return s.data.Put([]byte(name), b, nil)
+}
+
+// Delete the transaction config for the given name
+func (s *Store) Delete(name string) (err error) {
+	ok, err := s.Has(name)
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		return transaction.ErrTransactionNotFound
+	}
+
+	return s.data.Delete([]byte(name), nil)
+}
+
+// Iter iterates the transaction configs applying the callback for the name and config pairs.
+// Returning false causes the iteration to stop.
+func (s *Store) Iter(callback func(name string, config transaction.Config) (proceed bool)) (err error) {
+	iter := s.data.NewIterator(nil, nil)
+	defer iter.Release()
+
+	for iter.Next() {
+		var config transaction.Config
+		name := string(iter.Key())
+
+		err = json.Unmarshal(iter.Value(), &config)
+		if err != nil {
+			return err
+		}
+
+		if !callback(name, config) {
+			return
+		}
+	}
+	return iter.Error()
+}
